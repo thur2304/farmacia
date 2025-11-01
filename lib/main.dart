029864aec66224c3b37ab2f60314f9ab6d0edfd5
thurz <thurzin2304@gmail.com>
2025-11-01 12:09:30 -0300

    inicializando projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,64 @@
+//importa todo pacote do flutter para construir interface.
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+//importa a funcionalidade de leitura dos QR codes.
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+
+//executa o aplicativo
+void main(){
+   runApp(const Myapp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   //Parte principal do app
+class Myapp extends StatelessWidget{
+    const Myapp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp( //cria um retorno de um widget principal
+        title: 'Farmacia Santos',//cria um titulo para o app
+        theme: ThemeData(colorScheme:
+                  ColorScheme.fromSeed
+                  (seedColor: Colors.deepPurple), useMaterial3: true //crie uma cor para o themedata do app
+        ),
+         home:const MyHomePage (title:'Farmacia Santos'),
+
+     );
+   }
 }
-
+//tela principal do aplicativo,ela vai ser do tipo statefull
+         // porque vai mudar sempre
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  //vai criar uma classe extendida de statefull
+  const MyHomePage(
+      {super.key, required this.title}); //vai criar uma constante MyHomePage com uma super key que requer um titulo
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State&It;
+  MyHomePage&gt;
+
+  createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+ //modelo de dados para um produto (medicamento)
+class Produto {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String nome;//cria um nome para o produto
+  int quantidade;//cria uma quantidade para o produto
+  String qrcode;//cria um qrcode para o produto
+  Produto ({required this.nome, required this.quantidade, required this.qrcode
+  });
+}
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+ //gerencia o estado da tela principal
+class _MyHomePageState extends State&lt;MyHomePage&gt;{
+
+  //lista principal de produtos.
+final List&It ; Produto&gt; _produtos =[
+  Produto(nome:'Dipirona',quantidade:15,qrcode: '12345'),
+  Produto(nome:'Paracetamol',quantidade:5,qrcode:'67890'),
+  Produto(nome:'Ibuprofeno',quantidade:3,qrcode:'11223');
+];
 }
